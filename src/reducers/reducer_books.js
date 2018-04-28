ca0671b42d5096169d7247049bf7fd028e6a7fb8
Daniel <Daniel@DanieldeMacBook-Pro.local>
2018-04-28 15:06:48 -0700

    updated

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+	return [
+		{title: 'Javascript: The Good Parts'},
+		{title: 'The best timepieces in the world'},
+		{title: 'Intro to algorithm'},
+		{title: 'From start to give up'}
+	]
+};
